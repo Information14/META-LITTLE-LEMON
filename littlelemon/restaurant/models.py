18e835ecb4ed10fb8d16488a40527010f27723c2
Andrew Halilu <g.andrewhalilu71@gmail.com>
2024-12-09 14:58:54 +0100

    Meta Little Lemon Back End Developer Project

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -26,7 +26,6 @@ class Menu(models.Model):
    name = models.CharField(max_length=200) 
    price = models.IntegerField(null=False) 
    menu_item_description = models.TextField(max_length=1000, default='')
-   image = models.ImageField()
 
    def __str__(self):
       return self.name
